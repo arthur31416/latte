dada76be1b7a0577c12dacef5584edd33c10532c
Arthur Parmentier <arthur.jsx@gmail.com>
2016-08-30 20:06:21 +0200

    Updated gitignore

--- App/Config/Auth.js
+++ App/Config/Auth.js
@@ -1,6 +1,6 @@
 const auth = {
   client_id: '74179321441.74178175045',
-  secret: 'YOUR_SECRET',
+  secret: 'YOUR_SAFE_SECRET',
   scope: 'team%3Aread+users%3Aread', // https://api.slack.com/docs/oauth-scopes
   redirect_uri: 'latte://com.latte.ios' // https://dev.twitter.com/cards/mobile/url-schemes && must match the uri entered on Slack
 }
