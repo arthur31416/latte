35108ad1eaf1fd9976e628e2a67824eefe35798b
Arthur Parmentier <arthur.jsx@gmail.com>
2016-08-30 20:09:19 +0200

    fixed untracked files

--- App/Config/Auth.js
+++
@@ -1,8 +0,0 @@
-const auth = {
-  client_id: '74179321441.74178175045',
-  secret: 'YOUR_SAFE_SECRET',
-  scope: 'team%3Aread+users%3Aread', // https://api.slack.com/docs/oauth-scopes
-  redirect_uri: 'latte://com.latte.ios' // https://dev.twitter.com/cards/mobile/url-schemes && must match the uri entered on Slack
-}
-
-export default auth
